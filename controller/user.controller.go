130df32dd39c72957541246345568f16d3d1118c
renato.guimaraes <renato.guimaraes@novitatus.com>
2019-04-29 23:08:43 +0100

    implementing operations

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -2,51 +2,51 @@ package controller
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
-	"github.com/gocql/gocql"
 	"github.com/gorilla/mux"
 	"github.com/renatoaguimaraes/golang-microservice/model"
+	"github.com/renatoaguimaraes/golang-microservice/repository"
 )
-
-var session *gocql.Session
-
-func init() {
-	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "user"
-	session, _ = cluster.CreateSession()
-}
 
 // GetUsers return all users
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	iter := session.Query("SELECT id, firstname, lastname FROM users").Iter()
-	var users []model.User
-	var user model.User
-	for iter.Scan(&user.ID, &user.FirstName, &user.LastName) {
-		users = append(users, user)
-	}
+	users := repository.GetUsers()
 	json.NewEncoder(w).Encode(users)
 }
 
 // GetUser return a user by id
 func GetUser(w http.ResponseWriter, r *http.Request) {
-
+	id := mux.Vars(r)["id"]
+	user, _ := repository.GetUser(id)
+	if user.IsValid() {
+		json.NewEncoder(w).Encode(user)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
 
 // CreateUser crates a user
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var user model.User
+	id := mux.Vars(r)["id"]
+	user := model.User{ID: id}
 	_ = json.NewDecoder(r.Body).Decode(&user)
-	user.ID = params["id"]
-	if err := session.Query("INSERT INTO users (id, firstname, lastname) VALUES (?, ?, ?)", user.ID, user.FirstName, user.LastName).Exec(); err != nil {
-		log.Fatal(err)
+	if user.IsValid() {
+		repository.CreateUser(user)
+		json.NewEncoder(w).Encode(user)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
 	}
-	json.NewEncoder(w).Encode(user)
 }
 
 // DeleteUser deletes a user specific user
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	user, err := repository.GetUser(id)
+	if err == nil && user.IsValid() {
+		repository.DeleteUser(user.ID)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 
 }
--- model/user.model.go
+++ model/user.model.go
@@ -6,3 +6,7 @@ type User struct {
 	FirstName string `json:"firstname,omitempty"`
 	LastName  string `json:"lastname,omitempty"`
 }
+
+func (user *User) IsValid() bool {
+	return len(user.ID) > 0 && len(user.FirstName) > 0 && len(user.LastName) > 0
+}
