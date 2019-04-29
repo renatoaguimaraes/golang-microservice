8743fb33603e4823ee42015c650edb030e9f9da4
Renato Guimarães <renato.guimaraes@farfetch.com>
2019-04-29 18:25:38 +0100

    Add cassandra integration

--- controller/user.controller.go
+++ controller/user.controller.go
@@ -2,33 +2,36 @@ package controller
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
+	"github.com/gocql/gocql"
 	"github.com/gorilla/mux"
 	"github.com/renatoaguimaraes/golang-microservice/model"
 )
 
-var users []model.User
+var session *gocql.Session
 
 func init() {
-	users = append(users, model.User{ID: "renatoaguimaraes@gmail.com", FirstName: "Renato", LastName: "Guimarães"})
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "user"
+	session, _ = cluster.CreateSession()
 }
 
 // GetUsers return all users
 func GetUsers(w http.ResponseWriter, r *http.Request) {
+	iter := session.Query("SELECT id, firstname, lastname FROM users").Iter()
+	var users []model.User
+	var user model.User
+	for iter.Scan(&user.ID, &user.FirstName, &user.LastName) {
+		users = append(users, user)
+	}
 	json.NewEncoder(w).Encode(users)
 }
 
 // GetUser return a user by id
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, item := range users {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&model.User{})
+
 }
 
 // CreateUser crates a user
@@ -37,18 +40,13 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user model.User
 	_ = json.NewDecoder(r.Body).Decode(&user)
 	user.ID = params["id"]
-	users = append(users, user)
+	if err := session.Query("INSERT INTO users (id, firstname, lastname) VALUES (?, ?, ?)", user.ID, user.FirstName, user.LastName).Exec(); err != nil {
+		log.Fatal(err)
+	}
 	json.NewEncoder(w).Encode(user)
 }
 
 // DeleteUser deletes a user specific user
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range users {
-		if item.ID == params["id"] {
-			users = append(users[:index], users[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(users)
-	}
+
 }
--- main.go
+++ main.go
@@ -19,23 +19,21 @@ func main() {
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
 
-	r := mux.NewRouter()
-	r.Use(commonMiddleware)
-	r.HandleFunc("/user", controller.GetUsers).Methods("GET")
-	r.HandleFunc("/user/{id}", controller.GetUser).Methods("GET")
-	r.HandleFunc("/user/{id}", controller.CreateUser).Methods("POST")
-	r.HandleFunc("/user/{id}", controller.DeleteUser).Methods("DELETE")
+	router := mux.NewRouter()
+	router.Use(commonMiddleware)
+	router.HandleFunc("/user", controller.GetUsers).Methods("GET")
+	router.HandleFunc("/user/{id}", controller.GetUser).Methods("GET")
+	router.HandleFunc("/user/{id}", controller.CreateUser).Methods("POST")
+	router.HandleFunc("/user/{id}", controller.DeleteUser).Methods("DELETE")
 
 	srv := &http.Server{
-		Addr: "0.0.0.0:8000",
-		// Good practice to set timeouts to avoid Slowloris attacks.
+		Addr:         "0.0.0.0:8000",
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
-		Handler:      r, // Pass our instance of gorilla/mux in.
+		Handler:      router,
 	}
 
-	// Run our server in a goroutine so that it doesn't block.
 	go func() {
 		if err := srv.ListenAndServe(); err != nil {
 			log.Println(err)
@@ -46,19 +44,21 @@ func main() {
 	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
 	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
 	signal.Notify(c, os.Interrupt)
-
 	// Block until we receive our signal.
 	<-c
-
 	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), wait)
 	defer cancel()
 	// Doesn't block if no connections, but will otherwise wait
 	// until the timeout deadline.
-	srv.Shutdown(ctx)
+	go func() {
+		srv.Shutdown(ctx)
+	}()
 	// Optionally, you could run srv.Shutdown in a goroutine and block on
 	// <-ctx.Done() if your application should wait for other services
 	// to finalize based on context cancellation.
+	<-ctx.Done()
+
 	log.Println("shutting down")
 	os.Exit(0)
 }
